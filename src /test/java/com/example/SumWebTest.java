90dd2aac3143c9b4eefc2d4f4f4cadfbb18abc8d
KILBA <kilba2077@gmail.com>
2025-10-15 10:29:43 +0530

    Update SumWebTest.java

--- src /test/java/com/example/SumWebTest.java
+++ src /test/java/com/example/SumWebTest.java
@@ -34,7 +34,7 @@ public class SumWebTest {
     @Test
     public void testSumOfTwoNumbers() throws InterruptedException {
         // ✅ Use the correct file URL for your HTML in Jenkins workspace
-        String url = "file:///C:/ProgramData/Jenkins/.jenkins/workspace/MavenDemo/src/test/resources/sum.html";
+        String url = "file:///C:/ProgramData/Jenkins/.jenkins/workspace/MavenDemo/Experiment9-Repo/src/test/resources/sum.html";
         driver.get(url);
 
         // Explicit wait until the first input is present
